2e2bc0710e5d63f5f87c85fcf61eefcf45362243
shati <shatilova_ira@mail.ru>
2022-12-13 16:01:41 +0600

    1 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework 11");
+        System.out.println(" ");
+        System.out.println("Задание 1"); // вычислить является год високосным или нет
+        printLeapYearOrNot(2023);
+    }
+    public static boolean isLeapYear (int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+    public static void printLeapYearOrNot(int year) {
+        if (isLeapYear(year)) {
+            System.out.println(year + " год является високосным");
+            return;
+        }
+            System.out.println(year + " год не является високосным");
     }
 }
